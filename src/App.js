4d230d9b8aaf6b34f5a019f9a1d398624564f45c
dhirajpatil2346 <79779312+dhirajpatil2346@users.noreply.github.com>
2022-12-21 12:23:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
